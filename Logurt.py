f19333d170abae8bdff6a6718b254d2f6d0d7944
Logan Sparks <logan.sparks1999@gmail.com>
2018-02-13 13:13:34 -0500

    Question 6

--- Logurt.py
+++ Logurt.py
@@ -1,11 +1,12 @@
 def numbersix():
-	result = set(input("Give a list of numbers, please! ")) 
+	result = set(input("Gimme ya list of numbers homie! "))
+ 
 	dup_items = set()
 	uniq_items = []
 	for x in result:
-	if x not in dup_items:
-	uniq_items.append(x)
-	dup_items.add(x)
+		if x not in dup_items:
+			uniq_items.append(x)
+			dup_items.add(x)
 
 	print(dup_items)	
 
