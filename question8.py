d32963b1c1c520ba48cb68c48eb485d56f4ae114
Jack Finney <jackpfinney1@gmail.com>
2018-02-13 13:19:53 -0500

    question8

---
+++ question8.py
@@ -0,0 +1,12 @@
+print ("enter a")
+a = input()
+print ("enter b")
+b = input()
+print ("enter c")
+c = input()
+print ("enter d")
+d = input()
+
+sum = float(a) + float(b) + float(c) - float(d)
+print ("A+B+C-D equals",sum)
+
